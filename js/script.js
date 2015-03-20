14026c3674c9b37e0185ff3c32dde63d213414b9
divmom <vid.sigsoln@gmail.com>
2015-03-19 23:19:37 -0400

    Display reviews of books by Oliver Sacks in the NYTimes. Also display background image of address entered in form on top of page.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+/*
 function loadData() {
 
     var $body = $('body');
@@ -14,7 +15,7 @@ function loadData() {
     {
         $city = $city + "," + cityParts[i];
     }*/
-    
+  /*  
     var $streetPlusCity = $street + " " + $city;
 
 
@@ -59,4 +60,78 @@ function loadData() {
 
 $('#form-container').submit(loadData);
 
-// loadData();
+*/
+
+
+function displayBkGndImage(){
+    var $body = $('body');
+    var $street = document.getElementById('street').value
+    var $city = document.getElementById('city').value
+    var $streetPlusCity = $street + " " + $city;
+    var requestString = "https://maps.googleapis.com/maps/api/streetview?size=300x300&location=" + encodeURIComponent($streetPlusCity);
+    $body.append('<img class="bgimg" src=' + requestString+ '>');
+};
+
+function loadData(){
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $greeting = $('#greeting');
+    // clear out old data before new request
+    $wikiElem.text("");
+    // load streetview
+    displayBkGndImage();
+    loadNYTimesArticles();
+    // YOUR CODE GOES HERE!
+    return false;
+};
+
+function loadWikipediaArticles(){
+    return false;
+};
+
+var nytimes_success = function(data, status, jqXHR){
+        var nytimes_articles_ul = $('#nytimes-articles');
+        var numItems = data.num_results;
+        var results = data.results;
+        var book_list = document.getElementById('nytimes-articles');
+        // append empty line to ul - begin.
+        var listItem = document.createElement('listitem');
+        var textString = "\n";
+        listItem.innerText = textString;
+        book_list.appendChild(listItem);
+        // append empty line to ul - end
+
+        for(var i=0; i < numItems; i++)
+        {
+            var listItem = document.createElement('listitem');
+            var textString = "Review of " + results[i].book_title + " by " + results[i].byline + "\n";
+            listItem.innerText = textString;
+            book_list.appendChild(listItem);
+        }
+};
+
+var nytimes_error = function(){
+
+};
+
+function loadNYTimesArticles(){
+    //var nytimes_sacks = "http://api.nytimes.com/svc/books/v3/reviews.jsonp?author=Oliver Sacks&api-key=c262b76484d68f7da3ef47ee5432d47f:7:71609997&callback=books";
+    var nytimes_sacks = "http://api.nytimes.com/svc/books/v3/reviews.jsonp?author=Oliver Sacks"; 
+    $.ajax({
+            url: nytimes_sacks,
+            dataType: "jsonp",
+            type: "GET",
+            data: { "api-key": "c262b76484d68f7da3ef47ee5432d47f:7:71609997"},
+            jsonpCallback: "books",
+            success: nytimes_success
+          }); 
+    return false;
+};
+
+
+//$('#submit-btn').click(loadData);
+$('#form-container').submit(loadData);
+$('#my-form').submit(loadData);
+//$('#wikipedia-submit-btn').click(loadWikipediaArticles);
+
+//loadData();
